76b8f4a3c477c5968492707712c6acd136dd280c
askaaqib <askaaqib@gmail.com>
2019-02-28 13:22:15 +0500

    status changed

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -1,15 +1,31 @@
 @php
-$status_name = '';
-if ($old_status == 1 || $data['status'] == 1) { $status_name = 'Received';}
-if ($old_status == 2 || $data['status'] == 2) { $status_name = 'Assigned';}
-if ($old_status == 3 || $data['status'] == 3) { $status_name = 'On Hold';}
-if ($old_status == 4 || $data['status'] == 4) { $status_name = 'Completed';}
-if ($old_status == 5 || $data['status'] == 5) { $status_name = 'Submitted for Vetting';}
-if ($old_status == 6 || $data['status'] == 6) { $status_name = 'Invoiced';}
-if ($old_status == 7 || $data['status'] == 7) { $status_name = 'Paid';}
-if ($old_status == 8 || $data['status'] == 8) { $status_name = 'Cancelled';}
+$new_status_name = '';
+$old_status_name = '';
+if ($old_status == 1) { $old_status_name = 'Received';}
+if ($data['status'] == 1) { $new_status_name = 'Received';}
+
+if ($old_status == 2) { $old_status_name = 'Assigned';}
+if ($data['status'] == 2) { $new_status_name = 'Assigned';}
+
+if ($old_status == 3) { $old_status_name = 'On Hold';}
+if ($data['status'] == 3) { $new_status_name = 'On Hold';}
+
+if ($old_status == 4) { $old_status_name = 'Completed';}
+if ($data['status'] == 4) { $new_status_name = 'Completed';}
+
+if ($old_status == 5) { $old_status_name = 'Submitted for Vetting';}
+if ($data['status'] == 5) { $new_status_name = 'Submitted for Vetting';}
+
+if ($old_status == 6) { $old_status_name = 'Invoiced';}
+if ($data['status'] == 6) { $new_status_name = 'Invoiced';}
+
+if ($old_status == 7) { $old_status_name = 'Paid';}
+if ($data['status'] == 7) { $new_status_name = 'Paid';}
+
+if ($old_status == 8) { $old_status_name = 'Cancelled';}
+if ($data['status'] == 8) { $new_status_name = 'Cancelled';}
 @endphp
 
-Jobcard Status changed from {{ $status_name }} to {{ $status_name }}
+Jobcard Status changed from {{ $old_status_name }} to {{ $new_status_name }}
 
 Thank you
