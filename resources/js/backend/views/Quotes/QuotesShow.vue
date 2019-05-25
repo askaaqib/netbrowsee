2a2fac7631dab461f4afe78d4037d9a08699742c
askaaqib <askaaqib@gmail.com>
2019-05-25 12:25:35 +0500

    portrait

--- resources/js/backend/views/Quotes/QuotesShow.vue
+++ resources/js/backend/views/Quotes/QuotesShow.vue
@@ -1052,7 +1052,7 @@ export default {
         filename: 'myfile.pdf',
         // image: { type: 'png' },
         html2canvas: { dpi: 900, letterRendering: false },
-        jsPDF: { unit: 'cm', format: 'a3', orientation: 'p' }
+        jsPDF: { unit: 'cm', format: 'a4', orientation: 'p' }
       })
       var x = document.getElementById('hideprint')
       var y = document.getElementById('back')
